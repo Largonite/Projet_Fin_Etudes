75d54516b16e5426d722a69a30a1096b9d23a6c3
Romain Duvillier <romain.duvillier@student.vinci.be>
2017-02-06 16:16:17 +0100

    Commit of frontend of connection

--- Paimen/WebUI/Controllers/ConnectionController.cs
+++ Paimen/WebUI/Controllers/ConnectionController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LoginManagement; 
 
 namespace WebUI.Controllers
 {
     public class ConnectionController : Controller
     {
+        private ILoginManagement _service;
+        public ConnectionController()
+        {
+            this._service = new LoginManagementImpl();
+        }
 
         public ViewResult SignIn()
         {
@@ -17,6 +23,19 @@ namespace WebUI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult UserFromReg(string reg)
+        {
+            User res = this._service.SignIn(new User { RegNumber = int.Parse(reg), Password="" });
+            
+            return View("UserInformation",User);
+        }
+
+        public ViewResult UserInformation(User u)
+        {
+            return View(u);
         }
     }
 }
