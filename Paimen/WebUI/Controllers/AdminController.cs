ee91d434e7f28b489cd67b618dd179c724c30339
Marc <marc.deburlet@gmail.com>
2017-02-07 10:30:27 +0100

    AdminController

---
+++ Paimen/WebUI/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginManagement;
+
+namespace WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ILoginManagement _service;
+
+        public AdminController()
+        {
+            this._service = new LoginManagementImpl();
+        }
+
+        // GET: UserManager
+        public ActionResult UserManagement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddUserFromCSV(HttpPostedFileBase csv)
+        {
+            return View("UserManagement");
+        }
+    }
+}
